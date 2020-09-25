9106e21ce5db181c2e27fc8f82477db857c2e136
duyvippro1156 <maiduybigbang@gmail.com>
2020-09-26 00:43:42 +0700

    btLogin

---
+++ BTLogin/app/src/main/java/com/example/btlogin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.btlogin;
+
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+    }
+}
